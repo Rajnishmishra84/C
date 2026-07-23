26d76ba06bef7a80d6d861ff9eaf50cd0fb94b13
Rajnish <rm924581@gmail.com>
2026-07-23 18:41:27 +0530

    Function

--- Chapter5/pointer.c
+++ Chapter5/pointer.c
@@ -2,12 +2,12 @@
 
 int main() {
 
-     int a = 25;
-     int* x = &a;
-     printf("%p\n", x);
-
-     *x = 7;
-     printf("%d\n", a);
+     int a = 75;
+     int *ptr = &a;
+     printf("The address of pointer to a is: %p\n", &ptr);  // address of ptr...
+     printf("The address of a is: %p\n", &a);
+     printf("The address of a is: %p\n", ptr);
+     printf("The value of a is: %d\n", *ptr);
+     printf("The value of a is: %d", a);
      return 0;
-     
 }
