e4d00fa4fc271d8c7d45fde4f45ba0c8f2c1a614
Rajnish <rm924581@gmail.com>
2026-07-30 09:48:23 +0530

    Recursion

---
+++ Chapter6/powerfunctionlog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int powerlog(int a, int b) {
+     if(b==0) return 1;
+     int x = powerlog(a,b/2) * powerlog(a,b/2);
+     if(b%2==0) {
+         return x*x;
+     }else {
+          return x*x*a;
+     }
+
+}
+int main()
+{
+     int a, b;
+     printf("Enter base: ");
+     scanf("%d", &a);
+     printf("Enter power: ");
+     scanf("%d", &b);
+     int p = powerlog(a,b);
+     printf("%d raised to the power %d is %d: ", p);
+
+     return 0;
+}
