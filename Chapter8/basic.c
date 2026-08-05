8c90f542b54abea7bd1c74de4422afc6d576a126
Rajnish <rm924581@gmail.com>
2026-08-05 08:41:17 +0530

    string

---
+++ Chapter8/basic.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+     int ch = 'A';
+     int x = (int)ch;
+     printf("%c - %d ", x,x);
+     return 0;
+}
---
+++ Chapter8/string.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+     char str[] = "consider as a single character";
+     int i = 0;
+     while(ch[i]!='\0') {
+          printf("%c", str[i]);
+          i++;
+     } return 0;
+}
