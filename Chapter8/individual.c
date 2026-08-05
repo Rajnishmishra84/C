281d2133613ba61bf4c9cd6e8454871e521dd449
Rajnish <rm924581@gmail.com>
2026-08-05 08:49:32 +0530

    String

---
+++ Chapter8/individual.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+     char str[] = "collage wallah";
+     printf("%c \n", str[0]); //* Accessing individual character 
+     str[0] = 'm'; //* Modify individual character
+     int i = 0;
+     while(str[i]!='\0') {
+          printf("%c", str[i]);
+          i++;
+     } return 0;
+}
